5d2990915e6bbf49fb33bcc52ad3d5bd86b2e4e7
QUERIC <antoine.queric@ifremer.fr>
2022-01-12 11:26:11 +0100

    Restricted list of datasets for ICOS
    Waiting for ICOS to create an aggregated dataset for all datasets and add P01 parameters

--- src/marine_eov_broker/MarineRiBroker.py
+++ src/marine_eov_broker/MarineRiBroker.py
@@ -37,7 +37,7 @@ class MarineBroker:
                                           "SDC_MED_CLIM_TS_V2_s_whole_period",
                                           "SDC_NAT_CLIM_TS_V2_050_m", "SDC_NAT_CLIM_TS_V2_050_s"],
         "http://erddap.emso.eu/erddap": None,
-        "https://erddap.icos-cp.eu/erddap": None
+        "https://erddap.icos-cp.eu/erddap": ["icos11ss20211206"]
     }
 
     def __init__(self, erddap_servers=DEFAULT_ERDDAP_SERVERS):
